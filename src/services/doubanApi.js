4a03d1811425897d29441a305e2511f7182cdfdd
Jon <stftrack@gmail.com>
2020-02-18 09:20:16 +0800

    修改api请求的base_url,因为github-pages会找不到axios请求的资源地址

--- src/services/doubanApi.js
+++ src/services/doubanApi.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const movie_annual2018_base_url = `/fakeApi/ithil_j/activity/movie_annual2018`
+const movie_annual2018_base_url = `/douban-annual-react/fakeApi/ithil_j/activity/movie_annual2018`
 
 export const api_movie2018 = axios.create({
   baseURL: movie_annual2018_base_url,
